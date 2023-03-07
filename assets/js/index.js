5c2d665cd6df97707394705cda810c9e5db3a451
jessica <jessica7rm@gmail.com>
2023-03-07 01:05:09 -0300

    commit_1

---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+const Events = data.events
+
+let contenedorCards = document.getElementById("cards");
+console.log(contenedorCards.innerHTML)
+
+let Eventos = ""
+
+function eventosTodos(array) {
+    
+  for (let i = 0; i < array.length; i++) {
+    Eventos += `
+    <div class="card" style="width: 17rem;">
+    <img src="${array[i].image}" class="card-img-top fotocard" alt="${array[i].name}">
+    <div class="card-body">
+        <h5 class="card-title">${array[i].name}</h5>
+        <p class="card-text">${array[i].description}</p>
+        <div class="card3">
+            <p>Price $ ${array[i].price}</p>
+            <a href="./details.html?id=${array[i]._id}" class="btn btn-outline-secondary">More details</a>
+        </div>
+    </div>
+</div>`
+  }
+}
+
+eventosTodos(Events);
+contenedorCards.innerHTML = Eventos
+
+//console.table(Eventos)
+
+
+
+
+
+
+
+
+
+
